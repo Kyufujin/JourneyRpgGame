a2972a42e29be09b09e78d6e06ff380a544c289d
Kyufujiin <suuben15@gmail.com>
2021-10-23 20:16:14 +0200

    character

--- Main.cpp
+++ Main.cpp
@@ -14,6 +14,12 @@ int main(){
     Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
     Vector2 mapPos{0.0, 0.0};
 
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knightPos{
+        windowWidth/2.0f - 4.0f*(0.5f * (float)knight.width/6.0f),
+        windowHeight/2.0f - 4.0f*(0.5f * (float)knight.height)
+    };
+
 
     SetTargetFPS(60);
 
@@ -36,8 +42,13 @@ int main(){
         
         
         
-        
-        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+        // draw a map
+        DrawTextureEx(map, mapPos, 0.0, 3.0, WHITE);
+
+        // draw a character
+        Rectangle source{0.f, 0.f, (float)knight.width/6.f, (float)knight.height};
+        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
+        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
         EndDrawing();
     }
 
